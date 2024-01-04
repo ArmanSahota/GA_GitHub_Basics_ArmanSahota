704461b3302fe04529def1c5fa12ec549d964d12
ArmanSahota <109508805+ArmanSahota@users.noreply.github.com>
2024-01-03 19:59:07 -0800

    done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, Professor!");
             Console.WriteLine("It's nice to meet everyone");
-            Console.WriteLine("I'm Arman Sahota and I want to build I think i want to build a game with a lanague I don't know too well like java script. I know a little java but I suck at using it at the moment")
+            Console.WriteLine("I'm Arman Sahota and I want to build I think i want to build a game with a language I don't know too well like java script. I know a little java but I suck at using it at the moment")
 
         }
     }
