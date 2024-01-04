670cee3ef4092cd6c7c58e9c21b0838206a25c5b
ArmanSahota <109508805+ArmanSahota@users.noreply.github.com>
2024-01-03 19:48:30 -0800

    fixed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Class!");
+            Console.WriteLine("Hello, Professor!");
             Console.WriteLine("It's nice to meet everyone");
         }
     }
